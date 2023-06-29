1bea8d4331ab576a0bbf67e4082bc55b6190cb26
张晓甜 <zhangxt1@tiens.com>
2023-06-29 16:40:19 +0800

    first

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1688027765661_8914',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
